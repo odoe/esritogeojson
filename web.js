41ff95467ae7599f6ce9646dad2a85bb8db126b6
Rene Rubalcava <odoe@odoe.net>
2011-11-22 21:23:03 +0000

    added a test esrijson string

--- web.js
+++ web.js
@@ -21,13 +21,15 @@ app.configure('production', function(){
     app.use(express.errorHandler());
 });
 
-var jsonInput = "test in";
-var jsonOutput = "test out";
+var test = '{"displayFieldName":"LOWPARCELID","fieldAliases":{"LOWPARCELID":"Lowest Parcel Identification Number"},"geometryType":"esriGeometryPolygon","spatialReference":{"wkid":4326},"fields":[{"name":"LOWPARCELID","type":"esriFieldTypeString","alias":"Lowest Parcel Identification Number","length":30}],"features":[{"attributes":{"LOWPARCELID":"1902226080"},"geometry":{"rings":[[[-83.231458627236648,42.617199350582993],[-83.231635236861806,42.617378536183793],[-83.231669512792919,42.617422957559519],[-83.231193468607543,42.617678900083554],[-83.230976370344521,42.617458631003402],[-83.231458627236648,42.617199350582993]]]}}]}';
+
+var jsonInput = test;
+var jsonOutput = "this will be GeoJSON";
 
 app.get('/', function(req, res){
     console.log('start of app');
     res.render('index', {
-            title: 'EsriJSON to GeoJSON',
+            title: 'EsriJSON to GeoJSON (experimental): Only works on Polygons for now',
             locals: { jsonIn: jsonInput, jsonOut: jsonOutput }
         });
 });
@@ -45,5 +47,5 @@ app.post('/parse/', function(req, res) {
 if (!module.parent) {
     var port = process.env.PORT || 3000;
     app.listen(port);
-    console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env)
+    console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 }

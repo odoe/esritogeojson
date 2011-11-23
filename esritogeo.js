5d15eba39b9d9510bd0595af7ef7bd8936e212e8
Rene Rubalcava <odoe@odoe.net>
2011-11-23 03:37:27 +0000

    expanding geoJSON algorithm

--- esritogeo.js
+++ esritogeo.js
@@ -23,41 +23,74 @@ var jsonToObject = function(stringIn) {
     return data;
 };
 
+// still not sure on how to translate some of these types
 var parseGeometryType = function(type) {
-    if (type === "esriGeometryPolygon") {
+    if (type === "esriGeometryPoint") {
+        return "Point";
+    } else if (type === "esriGeometryMultipoint") {
+        return "MultiPoint";
+    } else if (type === "esriGeometryPolyline") {
+        return "LineString";
+    } else if (type === "esriGeometryPolygon") {
         return "Polygon";
+    } /* else if (type === "esriGeometryPolygon") {
+        return "MultiLineString";
+    } else if (type === "esriGeometryPolygon") {
+        return "MultiPolygon";
+    }*/ else {
+        return "Empty";
     }
-    return "none";
+};
+
+var featureToGeo = function(feature, geomType) {
+    var geometry = {};
+    geometry.type = geomType;
+    
+    // grab the rings to coordinates
+    var geom = feature.geometry;
+    var coordinates = {};
+    if (geom.rings !== "undefined") {
+        coordinates = geom.rings;
+    } else if (geom.paths !== "undefined") {
+        coordinates = geom.paths;
+    }
+    geometry.coordinates = coordinates;
+    
+    // convert attributes to properties
+    var properties = {};
+    var attr = feature.attributes;
+    for (var field in attr) {
+        properties[field] = attr[field];
+    }
+    
+    var geoJS = {};
+    geoJS.geometry = geometry;
+    geoJS.properties = properties;
+    
+    return geoJS;
 };
 
 var deserialize = function(js, callback) {
     console.log("begin parsing json");
-    var geoJS = {};
+    
     var o = jsonToObject(js);
     var result;
-    if (null != o) {
+    if (null !== o) {
         var geomType;
         geomType = parseGeometryType(o.geometryType);
         
-        // prepare the main parts of the GeoJSON
-        var geometry = {};
-        geometry.type = geomType;
-        
-        // grab the rings to coordinates
-        var coordinates = o.features[0].geometry.rings;
-        geometry.coordinates = coordinates;
-        
-        // convert attributes to properties
-        var properties = {};
-        var attr = o.features[0].attributes;
-        for (var field in attr) {
-            properties[field] = attr[field];
+        var features = [];
+        for (var i = 0, feature = {}; feature = o.features[i]; i++) {
+            // prepare the main parts of the GeoJSON
+            var feat = featureToGeo(feature, geomType);
+            features.push(feat);
         }
         
-        geoJS.geometry = geometry;
-        geoJS.properties = properties;
+        var featColl = {};
+        featColl.type = "FeatureCollection";
+        featColl.features = features;
         
-        result = JSON.stringify(geoJS, function(key, value) {
+        result = JSON.stringify(featColl, function(key, value) {
             if (typeof value === 'number' && !isFinite(value)) {
                 return String(value);
             }
